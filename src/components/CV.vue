8333a448ce5c039f20e2006b9ff4afb7b3e20bcd
Alex Hoang Hai Nguyen <hoanghai92@hotmail.com>
2019-03-17 09:42:51 +0100

    add CVArea component

---
+++ src/components/CV.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>Ola Norman</h1>
+    <img
+      src="https://media.licdn.com/dms/image/C4E03AQFuI-9ceWkKoA/profile-displayphoto-shrink_200_200/0?e=1553126400&v=beta&t=ml21h12q2Owvl-2vywvuHjcs3rWvwCFw0h65YLZQfZY"
+      height="150"
+      width="150"
+    >
+    <CVArea header="Erfaringer" title="Amazon" subTitle="CEO" text="Beste CEO Amazon har hatt"/>
+    <CVArea header="Verv" title="MC Donalds" subTitle="Servicearbeider" text="Årets ansatt 7 år på rad"/>
+    <CVArea header="Utdanning" title="NTNU" subTitle="Fulgetitter" text="Ekspert på fulger"/>
+  </div>
+</template>
+
+<script>
+import CVArea from "./CVArea.vue";
+
+export default {
+  name: "CV",
+  components: {
+    CVArea
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  width: 60%;
+  align-self: center;
+}
+
+img {
+  border-radius: 50%;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/CVArea.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="cvArea">
+    <h2>{{header}}</h2>
+    <div class="content-container">
+      <h3>{{title}}</h3>
+      <h4>{{subTitle}}</h4>
+      <p>{{text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CVArea",
+  props: {
+    header: String,
+    title: String,
+    subTitle: String,
+    text: String
+  }
+};
+</script>
+
+<style scoped>
+.cvArea {
+  display: flex;
+  flex-direction: column;
+}
+
+h2 {
+  align-self: flex-start;
+  font-size: 16px;
+  margin: 0;
+  margin-bottom: 5px;
+}
+
+h4 {
+  font-weight: normal;
+}
+
+p {
+  font-size: 14px;
+  font-weight: 300;
+}
+
+h3,
+h4,
+p {
+  margin: 0;
+  margin-bottom: 5px;
+}
+
+.content-container {
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 15px;
+  margin-bottom: 20px;
+}
+</style>
